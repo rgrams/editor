70eccff522f31ec5c115b66921d02287752f3697
rossgrams <ross.grams@gmail.com>
2019-10-07 07:33:01 -0400

    Gui_Script: Use preset pros list for obj class. Handle tables (like pos).

--- gui_script.lua
+++ gui_script.lua
@@ -1,6 +1,12 @@
 
 local script = {}
 
+local baseX, baseY = 550, 0
+local yIncr = 15
+
+local classProperties = {
+	Object = { "name", "path", "visible", "pos", "angle", "sx", "sy" }
+}
 
 function script.init(self)
 	self.debugDraw = false
@@ -13,15 +19,23 @@ end
 function script.draw(self)
 	local o = self.obj
 	if o then
+		local class = o.className
+		local props = classProperties[class] or classProperties.Object
 		love.graphics.setColor(1, 1, 1, 1)
-		local sx, sy = 600, 300
-		local y = 15
-		love.graphics.print("path = " .. o.path, sx, sy + y*-1)
-		love.graphics.print("pos.x = " .. o.pos.x, sx, sy + y*0)
-		love.graphics.print("pos.y = " .. o.pos.y, sx, sy + y*1)
-		love.graphics.print("angle = " .. o.angle, sx, sy + y*2)
-		love.graphics.print("scale-x = " .. o.sx, sx, sy + y*3)
-		love.graphics.print("scale-y = " .. o.sy, sx, sy + y*4)
+		for i,k in pairs(props) do
+			local v = o[k]
+			local s
+			if type(v) == "table" then
+				s = tostring(k) .. " = { "
+				for _k,_v in pairs(v) do
+					s = s .. tostring(_k) .. " = " .. tostring(_v) .. ", "
+				end
+				s = string.sub(s, 1, -3) .. " }"
+			else
+				s = tostring(k) .. " = " .. tostring(v)
+			end
+			love.graphics.print(s, baseX, baseY + i * yIncr)
+		end
 	end
 end
 
