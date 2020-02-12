6f3372759263c6b040e7d2d90bd54da25017eecc
rossgrams <ross.grams@gmail.com>
2020-02-12 10:36:08 -0500

    Input-Manager: Delay add/remove during input calls. - Fixes an infinite loop issue.

--- input-manager.lua
+++ input-manager.lua
@@ -5,8 +5,17 @@ local M = {}
 
 local objStack = {}
 
+-- Can cause infinite loops and other wacky behavior if you modify the stack while
+-- iterating through it, so delay all add-removes until the input is dealt with.
+local isDuringCall = false
+local delayedAddRemoves = {}
+
 function M.add(obj, pos)
 	pos = pos or "top"
+	if isDuringCall then
+		table.insert(delayedAddRemoves, {"add", obj, pos})
+		return
+	end
 	if pos == "top" then
 		table.insert(objStack, 1, obj) -- First on stack is the "top" - the first to get input.
 	else
@@ -15,11 +24,27 @@ function M.add(obj, pos)
 end
 
 function M.remove(obj)
+	if isDuringCall then
+		table.insert(delayedAddRemoves, 1, {"remove", obj}) -- List in reverse order so we can remove them as we go.
+		return
+	end
 	for i,v in ipairs(objStack) do
 		if v == obj then
 			table.remove(objStack, i)
 			return
 		end
+	end
+end
+
+function doDelayedCalls()
+	for i=#delayedAddRemoves,1,-1 do
+		local v = delayedAddRemoves[i]
+		if v[1] == "add" then
+			M.add(v[2], v[3])
+		else
+			M.remove(v[2])
+		end
+		delayedAddRemoves[i] = nil
 	end
 end
 
@@ -41,17 +66,24 @@ local function call(obj, funcName, ...)
 end
 
 function M.mouseMoved(x, y, dx, dy)
+	isDuringCall = true
 	for i,obj in ipairs(objStack) do
 		local consumed = call(obj, "mouseMoved", x, y, dx, dy)
-		if consumed then  return  end
+		if consumed then  break  end
 	end
+	isDuringCall = false
+	doDelayedCalls()
 end
 
 function M.input(name, value, change)
+	isDuringCall = true
+	shouldRedraw = true
 	for i,obj in ipairs(objStack) do
 		local consumed = call(obj, "input", name, value, change)
-		if consumed then  return  end
+		if consumed then  break  end
 	end
+	isDuringCall = false
+	doDelayedCalls()
 end
 
 return M
