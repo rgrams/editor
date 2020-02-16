89e7c89d61590c128b30e3fdcb6efc9e344596da
rossgrams <ross.grams@gmail.com>
2020-02-16 08:02:06 -0500

    Obj-Prop: Switch to key-value, add functions, add construct args.

--- object/editable-object-properties.lua
+++ object/editable-object-properties.lua
@@ -7,90 +7,131 @@ local setget = require "object.object-prop-set-getters"
 local set, get = setget.set, setget.get
 local defAssets = require "defaultAssets.list"
 
+-- local NO_DEFAULT = {}
 local NO_DEFAULT = constructArgs.NO_DEFAULT
+M.NO_DEFAULT = NO_DEFAULT
+M.classList = { "Object", "Sprite", "Text", "Quad", "World" }
+M.stringToClass = {
+	Object = Object, Sprite = Sprite, Text = Text, Quad = Quad, World = World
+}
+M.constructArgs = {}
 
 function M.getSpecs(className, propName)
-	local t = M[className] or M.Object
-	for i,v in ipairs(t) do
-		if v[1] == propName then
-			return v
-		end
-	end
+	local classPropList = M[className] or M.Object
+	return classPropList[propName]
+end
+
+function M.getDefault(className, key, subKey)
+	local propData = (M[className] or M.Object)[key]
+	local defaultVal = propData[1]
+	if subKey then  defaultVal = defaultVal[subKey]  end
+	return defaultVal, propData[5]
+end
+
+function M.getValue(obj, key, subKey)
+	local propList = M[obj.className] or M.Object
+	local getter = propList[key][4] or get.default
+	return getter(obj, key, subKey)
+end
+
+function M.setValue(obj, key, val, subKey)
+	local propList = M[obj.className] or M.Object
+	local setter = propList[key][3] or set.default
+	setter(obj, key, val, subKey)
 end
 
 M.displayNames = { kx = "shear x", ky = "shear y" }
 
 -- Properties that all objects have:
 M.universalProperties = {
-	-- { key, defaultVal, type, setter, getter }
-	{ "name", nil, "string"},
-	{ "path", nil, "read-only" },
-	{ "layer", nil, "string" }, -- Should be multiple-choice at some point.
-	{ "script", nil, "list" },
+	-- key = { defaultVal, type, setter, getter }
+	name = { nil, "string"},
+	path = { nil, "read-only" },
+	layer = { nil, "string" }, -- Should be multiple-choice at some point.
+	script = { nil, "list" },
 }
 
 M.Object = {
-	-- { key, defaultVal, type, setter, getter }
-	{ "pos", {x=0,y=0}, "vector2" },
-	{ "angle", 0, "number" },
-	{ "sx", 0, "number" },
-	{ "sy", 0, "number" },
-	{ "kx", 0, "number" },
-	{ "ky", 0, "number" }
+	-- key = { defaultVal, type, setter, getter }
+	pos = { {x=0,y=0}, "vector2" },
+	angle = { 0, "number" },
+	sx = { 0, "number" },
+	sy = { 0, "number" },
+	kx = { 0, "number" },
+	ky = { 0, "number" }
+}
+M.constructArgs.Object = {
+	{"pos", "x"}, {"pos", "y"}, {"angle"},
+	{"sx"}, {"sy"}, {"kx"}, {"ky"}
 }
 
 M.Sprite = {
-	-- { key, defaultVal, type, setter, getter }
-	{ "image", NO_DEFAULT, "string", set.imageData, get.assetParams, defAssets.image},
-	{ "pos", {x=0,y=0}, "vector2" },
-	{ "angle", 0, "number" },
-	{ "sx", 0, "number" },
-	{ "sy", 0, "number" },
-	{ "color", {1,1,1,1}, "color"},
-	{ "ox", 0, "number", set.imgOffsetFraction, get.imgOffsetFraction },
-	{ "oy", 0, "number", set.imgOffsetFraction, get.imgOffsetFraction },
-	{ "kx", 0, "number" },
-	{ "ky", 0, "number" }
+	-- key = { defaultVal, type, setter, getter }
+	image = { NO_DEFAULT, "string", set.imageData, get.assetParams, defAssets.image},
+	pos = { {x=0,y=0}, "vector2" },
+	angle = { 0, "number" },
+	sx = { 0, "number" },
+	sy = { 0, "number" },
+	color = { {1,1,1,1}, "color"},
+	ox = { 0, "number", set.imgOffsetFraction, get.imgOffsetFraction },
+	oy = { 0, "number", set.imgOffsetFraction, get.imgOffsetFraction },
+	kx = { 0, "number" },
+	ky = { 0, "number" }
+}
+M.constructArgs.Sprite = {
+	{"image"}, {"pos", "x"}, {"pos", "y"}, {"angle"}, {"sx"}, {"sy"},
+	{"color"}, {"ox"}, {"oy"}, {"kx"}, {"ky"},
 }
 
 M.Text = {
-	-- { key, defaultVal, type, setter, getter }
-	{ "text", "", "string"},
-	{ "font", NO_DEFAULT, "font", set.font, get.assetParams, defAssets.font},
-	{ "pos", {x=0,y=0}, "vector2" },
-	{ "angle", 0, "number" },
-	{ "wrapLimit", nil, "number" },
-	{ "hAlign", "left", "string" },
-	{ "sx", 0, "number" },
-	{ "sy", 0, "number" },
-	{ "kx", 0, "number" },
-	{ "ky", 0, "number" }
+	-- key = { defaultVal, type, setter, getter }
+	text = { "", "string"},
+	font = { NO_DEFAULT, "font", set.font, get.assetParams, defAssets.font},
+	pos = { {x=0,y=0}, "vector2" },
+	angle = { 0, "number" },
+	wrapLimit = { nil, "number" },
+	hAlign = { "left", "string" },
+	sx = { 0, "number" },
+	sy = { 0, "number" },
+	kx = { 0, "number" },
+	ky = { 0, "number" }
+}
+M.constructArgs.Text = {
+	{"text"}, {"font"}, {"pos", "x"}, {"pos", "y"}, {"angle"},
+	{"wrapLimit"}, {"hAlign"}, {"sx"}, {"sy"}, {"kx"}, {"ky"}
 }
 
 M.Quad = {
-	-- { key, defaultVal, type, setter, getter }
-	{ "image", NO_DEFAULT, "string", set.imageData, get.assetParams, defAssets.image },
-	{ "quad", NO_DEFAULT, "quad", set.quadParams, get.quadParams, defAssets.quad },
-	{ "pos", {x=0,y=0}, "vector2" },
-	{ "angle", 0, "number" },
-	{ "sx", 0, "number" },
-	{ "sy", 0, "number" },
-	{ "color", {1,1,1,1}, "color"},
-	{ "ox", 0, "number", set.imgOffsetFraction, get.imgOffsetFraction },
-	{ "oy", 0, "number", set.imgOffsetFraction, get.imgOffsetFraction },
-	{ "kx", 0, "number" },
-	{ "ky", 0, "number" }
+	-- key = { defaultVal, type, setter, getter }
+	image = { NO_DEFAULT, "string", set.imageData, get.assetParams, defAssets.image },
+	quad = { NO_DEFAULT, "quad", set.quadParams, get.quadParams, defAssets.quad },
+	pos = { {x=0,y=0}, "vector2" },
+	angle = { 0, "number" },
+	sx = { 0, "number" },
+	sy = { 0, "number" },
+	color = { {1,1,1,1}, "color"},
+	ox = { 0, "number", set.imgOffsetFraction, get.imgOffsetFraction },
+	oy = { 0, "number", set.imgOffsetFraction, get.imgOffsetFraction },
+	kx = { 0, "number" },
+	ky = { 0, "number" }
+}
+M.constructArgs.Quad = {
+	{"image"}, {"quad"}, {"pos", "x"}, {"pos", "y"}, {"angle"}, {"sx"}, {"sy"},
+	{"color"}, {"ox"}, {"oy"}, {"kx"}, {"ky"}
 }
 
 M.World = {
-	-- { key, defaultVal, type, setter, getter }
-	{ "xg", 0, "number", set.gravity, get.gravity },
-	{ "yg", 0, "number", set.gravity, get.gravity },
-	{ "sleep", false, "bool", set.sleepingAllowed, get.sleepingAllowed },
-	{ "disableBegin", false, "bool", set.worldCallbackEnabled, get.worldCallbackEnabled },
-	{ "disableEnd", false, "bool", set.worldCallbackEnabled, get.worldCallbackEnabled },
-	{ "disablePre", false, "bool", set.worldCallbackEnabled, get.worldCallbackEnabled },
-	{ "disablePost", false, "bool", set.worldCallbackEnabled, get.worldCallbackEnabled },
+	-- key = { defaultVal, type, setter, getter }
+	xg = { 0, "number", set.gravity, get.gravity },
+	yg = { 0, "number", set.gravity, get.gravity },
+	sleep = { false, "bool", set.sleepingAllowed, get.sleepingAllowed },
+	disableBegin = { false, "bool", set.worldCallbackEnabled, get.worldCallbackEnabled },
+	disableEnd = { false, "bool", set.worldCallbackEnabled, get.worldCallbackEnabled },
+	disablePre = { false, "bool", set.worldCallbackEnabled, get.worldCallbackEnabled },
+	disablePost = { false, "bool", set.worldCallbackEnabled, get.worldCallbackEnabled },
+}
+M.constructArgs.World = {
+	{"xg"}, {"yg"}, {"sleep"}, {"disableBegin"}, {"disableEnd"}, {"disablePre"}, {"disablePost"},
 }
 
 -- Property widgets:
