8453bd7aa24c48a7352731e41b6ca7a982cc67a1
rossgrams <ross.grams@gmail.com>
2020-02-16 10:38:00 -0500

    Theme.Fonts: Increase default font size to 14.

--- theme/fonts.lua
+++ theme/fonts.lua
@@ -1,6 +1,6 @@
 
 local fnt = {
-	openSans_Reg_12 = {"theme/font/OpenSans-Regular.ttf", 12},
+	openSans_Reg_12 = {"theme/font/OpenSans-Regular.ttf", 14},
 	panelTitle = {"theme/font/OpenSans-Regular.ttf", 14}
 }
 fnt.default = fnt.openSans_Reg_12
