82563ec7ded9cb1013a6cced88dd61eba28b2805
rossgrams <ross.grams@gmail.com>
2020-02-18 14:13:16 -0500

    Selection: Add `getEnclosureList` method.

--- Selection.lua
+++ Selection.lua
@@ -1,6 +1,18 @@
 
+local function getEnclosureList(self)
+	local enclosureList = {}
+	for obj,_ in pairs(self._) do
+		local enclosure = obj[PRIVATE_KEY]
+		table.insert(enclosureList, enclosure)
+	end
+	return enclosureList
+end
+
 local function new()
-	return { _ = {} } -- Separate content from reference.
+	return {
+		_ = {},  -- Separate content from reference.
+		getEnclosureList = getEnclosureList
+	}
 end
 
 return new
