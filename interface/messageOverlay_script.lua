4096be68b078c767e5ad2e1d40c6612ffd6ebdee
rossgrams <ross.grams@gmail.com>
2020-03-05 15:26:00 -0500

    Message-Overlay: Add different shapes, tweak visuals.

--- interface/messageOverlay_script.lua
+++ interface/messageOverlay_script.lua
@@ -3,28 +3,38 @@ local script = {}
 
 local fnt = require "theme.fonts"
 
-local DURATION = 3
-local ERROR_DURATION = 15
+local DEFAULT_DURATION = 3
 local BASE_X, BASE_Y = 0, 70
 local SHAPE_HEIGHT = 29
-local INNER_PADDING = 5
+local INNER_PADDING_X = 8
 local PADDING = 2
-local ROUND = 4
-local SHAPE_COLOR = {0.2, 1, 0.5, 0.3}
-local ERROR_SHAPE_COLOR = {1, 0, 0, 0.7}
+local ROUND = 12
+local DEFAULT_SHAPE_COLOR = {0.4, 0.75, 0.5, 0.3}
 local TEXT_COLOR = {1, 1, 1, 1}
+local WARNING_INDENT = 10
+
+local DURATION = {
+	error = 15,
+	warning = 5
+}
+
+local COLOR = {
+	error = {1, 0, 0, 0.7},
+	warning = {1, 0.5, 0, 0.3}
+}
 
 function script.init(self)
-	self.font = new.font(unpack(fnt.keyCast))
+	self.font = new.font(unpack(fnt.overlayMsg))
 	self.msgList = {}
 end
 
 local function newMessage(text, font, msgType)
-	local t = msgType == "error" and ERROR_DURATION or DURATION
+	local t = DURATION[msgType] or DEFAULT_DURATION
 	return {
 		text = text,
-		color = msgType == "error" and ERROR_SHAPE_COLOR or SHAPE_COLOR,
-		w = font:getWidth(text) + INNER_PADDING * 2,
+		type = msgType,
+		color = COLOR[msgType] or DEFAULT_SHAPE_COLOR,
+		w = font:getWidth(text) + INNER_PADDING_X * 2,
 		t = t,
 		dur = t
 	}
@@ -52,6 +62,20 @@ local function setColorWithAlphaMult(c, a)
 	love.graphics.setColor(r, g, b, a)
 end
 
+local function drawShape(msg, x, y)
+	if msg.type == "warning" then
+		love.graphics.rectangle("fill", x, y, msg.w, SHAPE_HEIGHT)
+	elseif msg.type == "error" then
+		local w2, h2 = msg.w/2, SHAPE_HEIGHT/2
+		local ind = WARNING_INDENT
+		local lt, rt, top, bot = x - ind, x + msg.w + ind, y, y + SHAPE_HEIGHT
+		love.graphics.polygon("fill", lt, top, rt, top, rt-ind, top+h2, lt+ind, top+h2, lt, top)
+		love.graphics.polygon("fill", lt, bot, lt+ind, top+h2, rt-ind, top+h2, rt, bot, lt, bot)
+	else
+		love.graphics.rectangle("fill", x, y, msg.w, SHAPE_HEIGHT, ROUND)
+	end
+end
+
 function script.draw(self)
 	if #self.msgList > 0 then
 		local X, Y = BASE_X, -self.h/2 + BASE_Y
@@ -61,9 +85,9 @@ function script.draw(self)
 			local x = X - msg.w/2
 			local colorMult = msg.t and msg.t/msg.dur or 1
 			setColorWithAlphaMult(msg.color, colorMult)
-			love.graphics.rectangle("fill", x, Y, msg.w, SHAPE_HEIGHT, ROUND)
+			drawShape(msg, x, Y)
 			setColorWithAlphaMult(TEXT_COLOR, colorMult)
-			love.graphics.print(msg.text, x + INNER_PADDING, Y + 1)
+			love.graphics.print(msg.text, x + INNER_PADDING_X, Y + 2)
 			Y = Y - (SHAPE_HEIGHT + PADDING)
 		end
 	end
--- theme/fonts.lua
+++ theme/fonts.lua
@@ -2,6 +2,7 @@
 local fnt = {
 	default = {"theme/font/OpenSans-Semibold.ttf", 12},
 	keyCast = {"theme/font/OpenSans-Semibold.ttf", 18},
+	overlayMsg = {"theme/font/OpenSans-Semibold.ttf", 16},
 	files = {"theme/font/OpenSans-Regular.ttf", 12},
 	panelTitle = {"theme/font/OpenSans-Semibold.ttf", 16},
 	viewportGrid = {"theme/font/OpenSans-Regular.ttf", 12}
