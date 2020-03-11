7a0e4f9526ea13e2357d92cbf6fcd46bfec3182d
rossgrams <ross.grams@gmail.com>
2020-03-11 09:18:16 -0400

    Viewport-Background: Update grid code with Josh's better code.

--- viewport/viewport_background.lua
+++ viewport/viewport_background.lua
@@ -3,15 +3,14 @@ local script = {}
 
 local fnt = require "theme.fonts"
 
-local snapIncrement = 8
-local desiredMaxGridLines = 48
-local bigGridEvery = 4
-local gridNumberScale = 1
 local gridColor = SETTINGS.gridColor
-local bigGridColor = SETTINGS.bigGridColor
-local gridNumberColor = SETTINGS.gridNumberColor
+local emphasizeColor = SETTINGS.bigGridColor
+local numberColor = SETTINGS.gridNumberColor
 local yAxisColor = SETTINGS.yAxisColor
 local xAxisColor = SETTINGS.xAxisColor
+
+local maxGridLines = 48
+local emphasizeEvery = 4
 
 local viewportPath = "/root/mainColumn/mainRow/editScenePanel/VPColumn/Viewport"
 
@@ -20,71 +19,73 @@ function script.init(self)
 	self.font = new.font(unpack(fnt.viewportGrid))
 end
 
-local function getSmallestPowerOf2(x)
-	local count = 0
-	while math.ceil(x) > 1.99 do
-		x = x / 2
-		count = count + 1
+local function nextLargerPower(x, base)
+	if x < 1 then
+		return 0
+	else
+		return math.ceil(math.log(x)/math.log(base))
 	end
-	return count
+end
+
+local function roundUpToPower(x, base)
+	return math.pow(base, nextLargerPower(x, base))
 end
 
 function script.draw(self)
+	-- Viewport is never rotated, so we don't need to do full bounds calculation.
+	local vp = self.vpNode
+	local s_lt, s_top = vp:toWorld(-vp.w/2, -vp.h/2)
+	local s_rt, s_bot = vp:toWorld(vp.w/2, vp.h/2)
+	local w_lt, w_top = Camera.current:screenToWorld(s_lt, s_top)
+	local w_rt, w_bot = Camera.current:screenToWorld(s_rt, s_bot)
+	local w_w, w_h = w_rt - w_lt, w_bot - w_top
+
+	local snapIncr = SETTINGS.translateSnapIncrement
+	local scale = 1 / Camera.current.zoom
+
+	local minSize = math.max(w_w, w_h) / maxGridLines
+	local n = roundUpToPower(minSize / snapIncr, 2)
+	local s = snapIncr * n -- Grid cell world-size.
+	local pad = s / 12 -- Number text padding/offset
+
+	-- Grid-increment bounds.
+	local lt, rt = math.floor(w_lt / s), math.ceil(w_rt / s)
+	local top, bot = math.floor(w_top / s), math.ceil(w_bot / s)
+
+	love.graphics.setLineWidth(scale)
 	love.graphics.setFont(self.font)
 
-	local vp = self.vpNode
-	local scrTLX, scrTLY = vp:toWorld(-vp.w/2, -vp.h/2)
-	local scrBRX, scrBRY = vp:toWorld(vp.w/2, vp.h/2)
-
-	local tlx, tly = Camera.current:screenToWorld(scrTLX, scrTLY)
-	local brx, bry = Camera.current:screenToWorld(scrBRX, scrBRY)
-	local w, h = brx - tlx, bry - tly
-
-	local scale = 1 / Camera.current.zoom
-	love.graphics.setLineWidth(scale)
-	local numScale = scale * gridNumberScale
-
-	-- Draw grid to fill screen.
-	local larger = math.max(w, h)
-	local k = larger / desiredMaxGridLines / snapIncrement
-	local power = getSmallestPowerOf2(k)
-	local viewSnapIncr = snapIncrement * math.pow(2, power)
-
-	local numSpacing = viewSnapIncr / 12
-
-	local xcount, ycount = math.ceil(w / viewSnapIncr), math.ceil(h / viewSnapIncr)
-	local roundTop = math.round(tly / viewSnapIncr) * viewSnapIncr
-	local roundLeft = math.round(tlx / viewSnapIncr) * viewSnapIncr
-	for i=1,ycount do
-		local y = roundTop + (i-1)*viewSnapIncr
-		if y/viewSnapIncr % bigGridEvery == 0 then
-			love.graphics.setColor(gridNumberColor)
-			love.graphics.print(y, 0 + numSpacing, y + numSpacing, 0, numScale, numScale)
-			love.graphics.print(y, tlx + numSpacing, y + numSpacing, 0, numScale, numScale)
-			love.graphics.setColor(bigGridColor)
-		else
+	-- Vertical lines
+	for x=lt,rt do
+		if x % emphasizeEvery ~= 0 then
 			love.graphics.setColor(gridColor)
+		else
+			love.graphics.setColor(numberColor)
+			love.graphics.print(x*s, x*s + pad, pad, 0, scale) -- ( text, x, y, r, sx)
+			love.graphics.print(x*s, x*s + pad, w_bot - 24*scale - pad, 0, scale)
+			love.graphics.setColor(emphasizeColor)
 		end
-		love.graphics.line(tlx, y, brx, y)
+		love.graphics.line(x*s, top*s, x*s, bot*s)
 	end
-	for i=1,xcount do
-		local x = roundLeft + (i-1)*viewSnapIncr
-		if x/viewSnapIncr % bigGridEvery == 0 then
-			love.graphics.setColor(gridNumberColor)
-			love.graphics.print(x, x + numSpacing, 0 + numSpacing, 0, numScale, numScale)
-			love.graphics.print(x, x + numSpacing, bry - 24*numScale + numSpacing, 0, numScale, numScale)
-			love.graphics.setColor(bigGridColor)
-		else
+
+	-- Horizontal lines
+	for y=top,bot do
+		if y % emphasizeEvery ~= 0 then
 			love.graphics.setColor(gridColor)
+		else
+			love.graphics.setColor(numberColor)
+			love.graphics.print(y*s, pad, y*s + pad, 0, scale) -- ( text, x, y, r, sx)
+			love.graphics.print(y*s, w_lt + pad, y*s + pad, 0, scale)
+			love.graphics.setColor(emphasizeColor)
 		end
-		love.graphics.line(x, tly, x, bry)
+		love.graphics.line(lt*s, y*s, rt*s, y*s)
 	end
 
 	-- Draw origin axis lines.
 	love.graphics.setColor(yAxisColor)
-	love.graphics.line(0, tly, 0, bry)
+	love.graphics.line(0, w_top, 0, w_bot)
 	love.graphics.setColor(xAxisColor)
-	love.graphics.line(tlx, 0, brx, 0)
+	love.graphics.line(w_lt, 0, w_rt, 0)
 
 	love.graphics.setLineWidth(1)
 end
